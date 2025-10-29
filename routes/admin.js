3e007e5c5c876d285eac9975362289aecaa2cc74
Dharaneeswar <dharaneeswar07@gmail.com>
2025-10-30 01:47:17 +0530

    fix: switched to bcryptjs for Render compatibility

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Admin = require('../models/Admin');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
